162b3806620a4a07d2279fb4323b15a1ad67f27f
Mauro.V <remanomauro@gmail.com>
2022-11-09 15:20:14 +0100

    add suspence

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,28 +1,14 @@
-import { use } from "react"
 
-
-
-
-//https://product-app-101-server.vercel.app/api/products
-async function getProducts() {
-    // getStaticProps
-    const res = await fetch("https://product-app-101-server.vercel.app/api/products")
-    return res.json()
-}
+import ProductList from "./ProductList";
+import { Suspense } from "react";
 
 export default function ProductsPage() {
-    const products = use(getProducts())
-
 return (
     <div className="page">
-    <h1>Hi there Products Page</h1>
-    <div>
-    { products.map(product =>
-        <div key={product.name}>
-        {product.name}
-        </div>
-        )}
-        </div>
-        </div>
+        <h1>Hi there Products Page</h1>
+        <Suspense fallback={<p>Loading Products</p>}>      
+        <ProductList />
+        </Suspense>
+    </div>
 )
 }
